e3b838a7693925a98d3ad3b937727bd363f71ac2
rmertkayaer <rmertkayaer@gmail.com>
2024-07-22 00:24:55 +0300

    ilk 10 bölüm

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,23 +1,19 @@
-import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from 'react'
+import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent, FormEvent } from 'react'
 
-type Props = {};
+interface Props  {
+  onClick : (e : SyntheticEvent) => void;
+  search : string | undefined;
+  handleChange : (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search :React.FC<Props> = (props: Props): JSX.Element => {
-  const[search,setSearch] = useState<string>("");
-  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e); 
-  }
+const Search : React.FC<Props> = ({onClick, search, handleChange}: Props): JSX.Element => {
 
-  const onClick = (e:SyntheticEvent) => {
-    console.log(e);
-  };
   return (  
     <div>
       <input value={search} onChange={(e) => handleChange(e)}></input>
       <button onClick={(e) => onClick(e)}/> 
     </div>  
-  )
-}
+  );
+};
 //zbab
-export default Search
+export default Search;
